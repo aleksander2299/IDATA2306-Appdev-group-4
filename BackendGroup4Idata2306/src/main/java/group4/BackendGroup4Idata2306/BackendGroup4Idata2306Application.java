7cf2873d919254b4173c3bdc177064f7658a1ab5
aleksander2299 <aleksander-nesvik@hotmail.com>
2025-03-11 14:33:29 +0100

    added spring initializr project since forgot to for first

---
+++ BackendGroup4Idata2306/src/main/java/group4/BackendGroup4Idata2306/BackendGroup4Idata2306Application.java
@@ -0,0 +1,13 @@
+package group4.BackendGroup4Idata2306;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGroup4Idata2306Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGroup4Idata2306Application.class, args);
+	}
+
+}
---
+++ BackendGroup4Idata2306/src/test/java/group4/BackendGroup4Idata2306/BackendGroup4Idata2306ApplicationTests.java
@@ -0,0 +1,13 @@
+package group4.BackendGroup4Idata2306;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGroup4Idata2306ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
