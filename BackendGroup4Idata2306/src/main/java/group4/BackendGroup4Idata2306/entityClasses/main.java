da2fedc8d8f137723a1b5965cd512f8b25bae776
tobiasnes <60387446+tobiasnes@users.noreply.github.com>
2025-03-11 15:06:37 +0100

    made main file runnable

--- BackendGroup4Idata2306/src/main/java/group4/BackendGroup4Idata2306/entityClasses/main.java
+++ BackendGroup4Idata2306/src/main/java/group4/BackendGroup4Idata2306/entityClasses/main.java
@@ -1,2 +1,13 @@
-package group4.BackendGroup4Idata2306.entityClasses;public class main {
+package group4.BackendGroup4Idata2306.entityClasses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
 }
