9462ffdea13b5fd7a3fee4005937efc76f47fab1
aleksander2299 <aleksander-nesvik@hotmail.com>
2025-03-17 14:35:10 +0100

    added full jpa entity class for room_provider from database model

--- src/main/java/group4/backend/entities/RoomProvider.java
+++ src/main/java/group4/backend/entities/RoomProvider.java
@@ -1,2 +1,61 @@
-package group4.backend.entities;public class roomProvider {
+package group4.backend.entities;
+
+import jakarta.persistence.*;
+
+/**
+ * class representing a room and provider relationship.
+ * IE a room and a provider listing the room with a price
+ */
+@Entity
+@Table(name = "room_provider")
+public class RoomProvider {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "room_provider_id", nullable = false)
+    private int roomProviderId;
+
+    @ManyToOne
+    @JoinColumn(name = "provider_id", nullable = false)
+    private Provider provider;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id", nullable = false)
+    private Room room;
+
+    @Column(name = "room_price")
+    private int roomPrice;
+
+
+    /**
+     * returns RoomProviderId
+     * @return the current RoomProviderId
+     */
+    public int getRoomProviderId() {
+        return roomProviderId;
+    }
+
+    /**
+     * sets RoomProviderId to a new RoomProviderId from parameter
+     * @param roomProviderId the new RoomProviderId
+     */
+    public void setRoomProviderId(int roomProviderId) {
+        this.roomProviderId = roomProviderId;
+    }
+
+    /**
+     * returns current RoomPrice
+     * @return the currentRoomPrice int
+     */
+    public int getRoomPrice() {
+        return roomPrice;
+    }
+
+    /**
+     * sets roomPrice to new roomPrice in parameter
+     * @param roomPrice the new roomPrice
+     */
+    public void setRoomPrice(int roomPrice) {
+        this.roomPrice = roomPrice;
+    }
 }
