1a0716b6ffd9e9fd23cfca7eee4081e1932648c5
tobiasnes <60387446+tobiasnes@users.noreply.github.com>
2025-03-17 19:35:22 +0100

    added source extra features foreign keys

--- src/main/java/group4/backend/entities/SourceExtraFeatures.java
+++ src/main/java/group4/backend/entities/SourceExtraFeatures.java
@@ -8,37 +8,37 @@ import jakarta.persistence.*;
 @Table(name = "source_extra_features")
 public class SourceExtraFeatures {
 
-    @Id
-    @Column(name = "feature", nullable = false)
-    private String feature;
+    @Id @ManyToOne
+    @JoinColumn(name = "feature", referencedColumnName = "feature", foreignKey = @ForeignKey(name = "FK_feature"))
+    private ExtraFeatures feature;
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "source_id", nullable = false)
-    private int sourceID;
+    @Id @ManyToOne
+    @JoinColumn(name = "source_id", referencedColumnName = "source_id", foreignKey = @ForeignKey(name = "FK_source_id"))
+    private Source sourceID;
 
     /**
      * returns feature
      * @return string feature
      */
-    public String getFeature() { return feature; }
+    public ExtraFeatures getFeature() { return feature; }
 
     /**
      * sets the feature to a new feature
      * @param feature the feature
      */
-    public void setFeature(String feature) {this.feature = feature; }
+    public void setFeature(ExtraFeatures feature) {this.feature = feature; }
 
     /**
      * returns source id
      * @return int source id
      */
-    public int getSourceID() { return sourceID; }
+    public Source getSourceID() { return sourceID; }
 
     /**
      * sets the source id to a new source id
      * @param sourceID the source id
      */
-    public void setSourceID(int sourceID) {this.sourceID = sourceID; }
+    public void setSourceID(Source sourceID) {this.sourceID = sourceID; }
 
 
 }
