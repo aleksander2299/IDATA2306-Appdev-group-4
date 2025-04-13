97cebfa4c69b115b66a66ddb908417d91b9bb940
aleksander2299 <aleksander-nesvik@hotmail.com>
2025-04-13 16:25:57 +0200

    created all but deletion from RoomController

--- src/main/java/group4/backend/controller/RoomController.java
+++ src/main/java/group4/backend/controller/RoomController.java
@@ -1,4 +1,45 @@
 package group4.backend.controller;
 
+import group4.backend.entities.Room;
+import group4.backend.service.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("/api/rooms")
 public class RoomController {
+
+
+    @Autowired
+    RoomService roomService;
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Room> getRoom(@PathVariable("id") int id) {
+        Optional<Room> roomOptional = roomService.getRoomById(id);
+        return roomOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+
+    @GetMapping("/rooms")
+    public ResponseEntity<List<Room>> getAllRooms(){
+        List<Room> rooms = roomService.getAllRooms();
+        if (rooms.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(rooms);
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Room> createRoom(@RequestBody Room room) {
+        Room newRoom = roomService.saveRoom(room);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newRoom);
+    }
 }
