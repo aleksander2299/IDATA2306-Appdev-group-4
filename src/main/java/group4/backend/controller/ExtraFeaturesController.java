6349e02ca5f854d018587f51df9e92588b224e66
tobiasnes <60387446+tobiasnes@users.noreply.github.com>
2025-04-23 14:32:50 +0200

    Made sourceExtrafeaturesController, fixed linking, made tests for it.
    Can now link together a feature and source using post, can get everything, can get spesific source, can get spesific feature.

--- src/main/java/group4/backend/controller/ExtraFeaturesController.java
+++ src/main/java/group4/backend/controller/ExtraFeaturesController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/extrafeatures")
+@RequestMapping("/api/extra_features")
 public class ExtraFeaturesController {
 
     @Autowired
