af91f5472b5f8111f586242e3d4bc585803449b4
aleksander2299 <aleksander-nesvik@hotmail.com>
2025-05-06 13:16:30 +0200

    made jwt service us customuserdetails to then when it generates token it sends the role in it so that frontend can decode jwt and get the role of the user for user based authority. (helped by chatgpt)

--- src/main/java/group4/backend/security/CustomUserDetails.java
+++ src/main/java/group4/backend/security/CustomUserDetails.java
@@ -28,7 +28,7 @@ public class CustomUserDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("ROLE_" + role));
+        return List.of(new SimpleGrantedAuthority(role));
     }
 
     @Override
