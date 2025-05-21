5b4e28b949d6676dfa2d4c09103dd1ec001cf3a5
aleksander2299 <aleksander-nesvik@hotmail.com>
2025-05-21 16:40:19 +0200

    Documentation : Added @operation swagger documentation to SourceController

--- src/main/java/group4/backend/controller/SourceController.java
+++ src/main/java/group4/backend/controller/SourceController.java
@@ -3,6 +3,8 @@ package group4.backend.controller;
 import group4.backend.entities.Room;
 import group4.backend.entities.Source;
 import group4.backend.service.SourceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -35,6 +37,14 @@ public class SourceController {
      * @param id the unique identifier of the Source to be retrieved
      * @return a ResponseEntity containing the Source if found, or a 404 Not Found response if the Source does not exist
      */
+    @Operation(
+            summary = "Get source from Source id ",
+            description = "gets a source from pathVariable id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "source found and returned"),
+                    @ApiResponse(responseCode = "404", description = "source not found")
+            }
+    )
     @GetMapping("/{id}")
     public ResponseEntity<Source> getSource(@PathVariable("id") int id) {
         Optional<Source> sourceOptional = sourceService.getSourceById(id);
@@ -47,6 +57,14 @@ public class SourceController {
      * @return a ResponseEntity containing a list of all Source objects if present;
      *         a 204 No Content response if no sources are found.
      */
+    @Operation(
+            summary = "return all sources in database",
+            description = "returns a list of all sources in the database",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "sources found and returned"),
+                    @ApiResponse(responseCode = "204", description = "no content")
+            }
+    )
     @GetMapping()
     public ResponseEntity<List<Source>> getAllSources(){
         List<Source> sources = sourceService.getAllSources();
@@ -63,6 +81,15 @@ public class SourceController {
      * @param source the Source entity to be created
      * @return a ResponseEntity containing the created Source entity with a 201 Created status code
      */
+    @Operation(
+            summary = "create a source put crud operation ",
+            description = "create a source based on requestBody of source data and save it to database",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "source created and saved"),
+                    @ApiResponse(responseCode = "403", description = "Expectations not met"),
+                    @ApiResponse(responseCode = "400", description = "Invalid input or bad request")
+            }
+    )
     @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping()
     public ResponseEntity<Source> createSource(@RequestBody Source source) {
@@ -75,9 +102,17 @@ public class SourceController {
      * @param sources the list of sources to post.
      * @return ResponseEntity if it succeeded or not.
      */
+    @Operation(
+            summary = "Method for posting bulk sources",
+            description = "posts a list of sources to database, requestBody of list of sources",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "sources created"),
+                    @ApiResponse(responseCode = "204", description = "empty list")
+            }
+    )
     @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping("/bulk")
-    public ResponseEntity<List<Source>> createRooms(@RequestBody List<Source> sources){
+    public ResponseEntity<List<Source>> createSources(@RequestBody List<Source> sources){
         if(sources.isEmpty()){
             return ResponseEntity.status(HttpStatus.NO_CONTENT).body(sources);
         }
@@ -92,6 +127,15 @@ public class SourceController {
      * @param id the id of the source to delete.
      * @return ResponseEntity if source was deleted or not.
      */
+    @Operation(
+            summary = "deletes a source from id",
+            description = "deletes a source that is gotten from source id ",
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "source deleted"),
+                    @ApiResponse(responseCode = "404", description = "source not found"),
+                    @ApiResponse(responseCode = "403",description = "expectations not met")
+            }
+    )
     @PreAuthorize("hasAnyRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Source> deleteSource(@PathVariable("id") int id) {
@@ -108,6 +152,14 @@ public class SourceController {
      * Deletes all sources in the database.
      * @return ResponseEntity if the list of sources is empty or if it did delete all.
      */
+    @Operation(
+            summary = "deletes all source from the database",
+            description = "deletes all sources",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "source deleted"),
+                    @ApiResponse(responseCode = "403",description = "expectations not met")
+            }
+    )
     @PreAuthorize("hasAnyRole('ADMIN')")
     @DeleteMapping()
     public ResponseEntity<Void> deleteAllSources() {
